6805cce19858bedc21d47c7d2a382585e40a5193
Krzysztof Prońko <pronko.krzysztof@gmail.com>
2022-12-19 22:23:38 +0100

    added basic view and component

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import { Inter } from '@next/font/google'
 import axios from 'axios'
 import { useState } from 'react'
 import {BsSearch} from 'react-icons/bs'
+import Weather from './components/Weather'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -12,14 +13,14 @@ export default function Home() {
   const [weather, setWeather] = useState('');
   const [loading, setLoading] = useState(false);
 
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=london&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}`;
 
   const fetchWeather = (e) => {
     e.preventDefault();
     setLoading(true);
     axios.get(url).then((resp) => {
       setWeather(resp.data);
-      console.log(resp.data)
+      // console.log(resp.data)
     })
     setLoading(false);
     setCity('');
@@ -32,7 +33,28 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <button onClick={fetchWeather}>click</button>
+      <div className='absolute top-0 left-0 right-0 bottom-0 bg-black/30 z-[1]'/>
+      <Image 
+        src='https://images.unsplash.com/photo-1592210454359-9043f067919b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80' 
+        alt='weather photo' 
+        layout='fill'
+        className='object-cover'
+        />
+      <div className='relative flex justify-between items-center max-w-[500px] w-full m-auto pt-4 text-white z-10'>
+        <form 
+        onSubmit={fetchWeather}
+        className='flex justify-between items-center w-full m-auto p-4 bg-white/20 border border-gray-200 text-white rounded-3xl'>
+          <div>
+            <input 
+            onChange={(e) => setCity(e.target.value)}
+            className='bg-transparent border-none focus:outline-none text-2xl placeholder:text-white/70' 
+            type='text' 
+            placeholder='search city'/>
+          </div>
+          <button onClick={fetchWeather}><BsSearch size={20}/></button>
+        </form>
+      </div>
+      {weather.main && <Weather data={weather}/>}
     </>
   )
 }
